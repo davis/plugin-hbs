f0212dd6c3351e4bffc08d3d30f5637b6e69ef29
Davis Kim <davis@blackbird.am>
2015-06-05 14:09:24 -0700

    fix for 0.16.0-beta

--- hbs.js
+++ hbs.js
@@ -3,5 +3,5 @@
 export function translate(load) {
   console.log(`compiling template: ${load.name}`);
   const escaped = load.source.replace('`', '\\`');
-  load.source = `import Handlebars from 'handlebars';\n\nexport default Handlebars.compile(\`${escaped}\`)`;
+  load.source = `module.exports = require('handlebars').compile(\`${escaped}\`);`;
 }
