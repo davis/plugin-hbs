047c7301739374fe503a536d2e2ef9f885cbdb5c
Davis Kim <dchk90@gmail.com>
2015-06-05 11:56:58 -0700

    Update hbs.js

--- hbs.js
+++ hbs.js
@@ -3,5 +3,5 @@
 export function translate(load) {
   console.log(`compiling template: ${load.name}`);
   const escaped = load.source.replace('`', '\\`');
-  return `import Handlebars from 'handlebars';\n\nexport default Handlebars.compile(\`${escaped}\`)`;
+  load.source = `import Handlebars from 'handlebars';\n\nexport default Handlebars.compile(\`${escaped}\`)`;
 }
