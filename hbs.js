b3fcd4c2d31f518cc4b708c5a6fc1e2fae6439c8
Davis Kim <davis@blackbird.am>
2015-06-08 16:48:58 -0700

    es5 friendly

--- hbs.js
+++ hbs.js
@@ -1,7 +1,14 @@
 'use strict';
 
 export function translate(load) {
-  console.log(`compiling template: ${load.name}`);
-  const escaped = load.source.replace('`', '\\`');
-  load.source = `module.exports = require('handlebars').compile(\`${escaped}\`);`;
+  console.log('compiling template:', load.name);
+  load.source = 'module.exports = require(\'handlebars\').compile(\'' + load.source
+  .replace(/(["\\])/g, '\\$1')
+  .replace(/[\f]/g, '\\f')
+  .replace(/[\b]/g, '\\b')
+  .replace(/[\n]/g, '\\n')
+  .replace(/[\t]/g, '\\t')
+  .replace(/[\r]/g, '\\r')
+  .replace(/[\u2028]/g, '\\u2028')
+  .replace(/[\u2029]/g, '\\u2029') + '\');';
 }
