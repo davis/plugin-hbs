6c453e1ad3454602e52091abda1825885ba50d2c
Davis Kim <davis@blackbird.am>
2016-09-06 16:50:51 -0700

    use System.normalizeSync to get proper handlebars runtime path

--- hbs.js
+++ hbs.js
@@ -4,7 +4,7 @@ import Handlebars from 'handlebars';
 
 export function translate(load) {
   let precompiled = Handlebars.precompile(load.source);
-  let handlebarsRuntimePath = 'handlebars/handlebars.runtime.js';
+  let handlebarsRuntimePath = System.normalizeSync('handlebars/handlebars.runtime.js', __moduleName);
 
   // in builds we want to embed the canonical name for the handlebars runtime
   if (System.getCanonicalName) {
@@ -15,9 +15,9 @@ export function translate(load) {
   if (load.metadata.format === 'esm') {
     output = `import {Handlebars} from '${handlebarsRuntimePath}'; \n export default Handlebars.template(${precompiled});`;
   } else if (load.metadata.format === 'amd') {
-    output = `define(['${handlebarsRuntimePath}'], function(Handlebars) { return Handlebars.template(${precompiled}); });`;
+    output = `define(['${handlebarsRuntimePath}'], function (Handlebars) { return Handlebars.template(${precompiled}); });`;
   } else {
-    output = `var Handlebars=require('${handlebarsRuntimePath}'); \n module.exports = Handlebars.template(${precompiled});`;
+    output = `var Handlebars = require('${handlebarsRuntimePath}'); \n module.exports = Handlebars.template(${precompiled});`;
   }
   load.source = output;
   return output;
